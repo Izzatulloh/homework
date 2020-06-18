01def533d96748ff9459534494137d755b6375ba
Izztulloh <Izzatulloh5566@gmail.com>
2020-06-18 23:01:38 +0500

    new project

---
+++ 1-vazifa.js
@@ -0,0 +1,18 @@
+do {
+    var a = prompt("soni to'g'rilab kiriting")
+    
+} while (isNaN(a) || a==0 ){
+
+} do {
+
+    var b = prompt("darajani to'g'rilab kirit")
+
+} while (isNaN(b) || b==0) {
+
+}
+    var c = a;
+
+    for (i = 1; i < b; i++) {
+        a= a*c;
+}   alert(a);
+
--- script.js
+++ script.js
@@ -1,35 +1,16 @@
-do {
-    var a = prompt("soni to'g'rilab kiriting")
-    
-} while (isNaN(a) || a==0 ){
-
-} do {
-
-    var b = prompt("darajani to'g'rilab kirit")
-
-} while (isNaN(b) || b==0) {
-
-}
-    var c = a;
-
-    for (i = 1; i < b; i++) {
-        a= a*c;
-}   alert(a);
-
-
-
-
 do{
 
     var  numberOfStairs = prompt("Son kiriting")
 
 }   while(numberOfStairs<=0){
     
-}   if(numberOfStairs==isNaN){
+}   
+if(numberOfStairs=isNaN){
 
-    numberOfStairs=5
+    numberOfStairs=5;
 
-}   do{
+}   
+do{
     
     var firstSymbol = prompt('birinchi simbol kiriting')
 
