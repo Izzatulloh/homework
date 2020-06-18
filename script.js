e91e63081b5629103c8ea4b51759a7c02557df69
Izztulloh <Izzatulloh5566@gmail.com>
2020-06-18 22:51:34 +0500

    assa

---
+++ script.js
@@ -0,0 +1,48 @@
+do {
+    var a = prompt("soni to'g'rilab kiriting")
+    
+} while (isNaN(a) || a==0 ){
+
+} do {
+
+    var b = prompt("darajani to'g'rilab kirit")
+
+} while (isNaN(b) || b==0) {
+
+}
+    var c = a;
+
+    for (i = 1; i < b; i++) {
+        a= a*c;
+}   alert(a);
+
+
+
+
+do{
+
+    var  numberOfStairs = prompt("Son kiriting")
+
+}   while(numberOfStairs<=0){
+    
+}   if(numberOfStairs==isNaN){
+
+    numberOfStairs=5
+
+}   do{
+    
+    var firstSymbol = prompt('birinchi simbol kiriting')
+
+}   while(!isNaN(firstSymbol)){
+
+}   do{
+    
+    var lastSymbol = prompt('oxirgi simbol kiritng')
+
+}   while(!isNaN(lastSymbol)){
+
+}   for(var i = 0;i < numberOfStairs; i++){
+        lastSymbol;
+        console.log(lastSymbol);
+        lastSymbol= firstSymbol+ lastSymbol;
+}
